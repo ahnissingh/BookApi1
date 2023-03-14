b091c0ece5225958eb02b6e557c21e744e2c20a5
ahnissingh <61423624+ahnissingh@users.noreply.github.com>
2023-03-14 16:34:48 +0530

    Delete src directory

--- src/main/java/com/ahnis/exceptions/ResourceNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package com.ahnis.exceptions;
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
